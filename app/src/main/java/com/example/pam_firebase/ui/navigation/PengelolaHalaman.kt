e8048073cb77d9a1494e33624c78f05a38cc84bb
Khaiqol <Khaiqol@gmail.com>
2025-01-12 14:47:04 +0700

    membuat pengelola halaman

---
+++ app/src/main/java/com/example/pam_firebase/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,40 @@
+package com.example.pam_firebase.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pam_firebase.ui.view.HomeScreen
+import com.example.pam_firebase.ui.view.HomeStatus
+import com.example.pam_firebase.ui.view.InsertView
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+        composable(DestinasiInsert.route) {
+            InsertView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
