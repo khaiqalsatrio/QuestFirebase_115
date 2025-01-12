f703c59378ce6c43d76a0ba46330369c7802fab9
Khaiqol <Khaiqol@gmail.com>
2025-01-12 13:10:40 +0700

    di class insert view model membuat var uiEvent, uiState, dan fun update State

--- app/src/main/java/com/example/pam_firebase/ui/viewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/pam_firebase/ui/viewModel/InsertViewModel.kt
@@ -1,6 +1,29 @@
 package com.example.pam_firebase.ui.viewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.pam_firebase.model.Mahasiswa
+import com.example.pam_firebase.repository.MhsRepository
+
+class InsertViewModel(
+    private val mhs: MhsRepository
+) : ViewModel() {
+
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    // memperbarui state berdasarkan input pengguna
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent,
+        )
+    }
+}
 
 sealed class FormState {
     object Idle : FormState()
